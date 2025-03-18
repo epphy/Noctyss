4d89c7c4d4ae03dbc8153fbaec5ff968145201f0
Vladimir <139114084+ephyrriz@users.noreply.github.com>
2025-03-19 01:07:37 +0400

    bb

--- src/main/java/ru/vladimir/votvproduction/api/EventAPI.java
+++ src/main/java/ru/vladimir/votvproduction/api/EventAPI.java
@@ -1,4 +1,21 @@
 package ru.vladimir.votvproduction.api;
 
+import lombok.Getter;
+import ru.vladimir.votvproduction.event.WorldStateManager;
+import ru.vladimir.votvproduction.utility.LoggerUtility;
+
 public class EventAPI {
+    @Getter
+    private static WorldStateManager worldStateManager;
+
+    private EventAPI() {}
+
+    public static void initialise(WorldStateManager worldStateManager) {
+        if (EventAPI.worldStateManager == null) {
+            EventAPI.worldStateManager = worldStateManager;
+            LoggerUtility.info(EventAPI.class, "EventAPI initialised");
+            return;
+        }
+        LoggerUtility.info(EventAPI.class, "EventAPI is already initialised");
+    }
 }
