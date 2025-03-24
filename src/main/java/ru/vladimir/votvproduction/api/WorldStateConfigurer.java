f88eaf3501c69bceb3d4c8ac08de1fff65f17438
Vladimir <139114084+ephyrriz@users.noreply.github.com>
2025-03-24 15:06:49 +0400

    api package fully reworked

--- src/main/java/ru/vladimir/votvproduction/api/WorldStateConfigurer.java
+++ src/main/java/ru/vladimir/votvproduction/api/WorldStateConfigurer.java
@@ -1,13 +1,29 @@
 package ru.vladimir.votvproduction.api;
 
 import lombok.RequiredArgsConstructor;
+import org.bukkit.World;
 import ru.vladimir.votvproduction.config.GeneralConfig;
+import ru.vladimir.votvproduction.event.EventType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class WorldStateConfigurer {
     private final GeneralConfig generalConfig;
 
     WorldStateManager configure() {
+        final Map<World, WorldState> worldStates = new HashMap<>();
 
+        for (final Map.Entry<World, List<EventType>> entry : generalConfig.getAllowedEventWorlds().entrySet()) {
+
+            final World world = entry.getKey();
+            final List<EventType> allowedEvents = entry.getValue();
+
+            worldStates.put(world, new WorldState(world, new HashMap<>(), allowedEvents));
+        }
+
+        return new WorldStateManager(worldStates);
     }
 }
--- src/main/java/ru/vladimir/votvproduction/config/GeneralConfig.java
+++ src/main/java/ru/vladimir/votvproduction/config/GeneralConfig.java
@@ -1,6 +1,16 @@
 package ru.vladimir.votvproduction.config;
 
+import lombok.Getter;
+import org.bukkit.World;
+import ru.vladimir.votvproduction.event.EventType;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
 public class GeneralConfig implements Config {
+    private Map<World, List<EventType>> allowedEventWorlds;
+
     @Override
     public void load() {
 
