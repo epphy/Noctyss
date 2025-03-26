c06bd116e2bad5e5b0ba5fc7b515f4101001b5d3
Vladimir <gamedimaspro@gmail.com>
2025-03-26 16:59:48 +0400

    Done w/ serializer & storage, and now working on service. Instead of notification rules, finna use the classes names

--- src/main/java/ru/vladimir/votvproduction/event/modules/notification/storage/PlayerNotificationSerializer.java
+++ src/main/java/ru/vladimir/votvproduction/event/modules/notification/storage/PlayerNotificationSerializer.java
@@ -1,21 +1,56 @@
 package ru.vladimir.votvproduction.event.modules.notification.storage;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import ru.vladimir.votvproduction.event.EventType;
 import ru.vladimir.votvproduction.event.modules.notification.NotificationRule;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-public class PlayerNotificationSerializer {
+class PlayerNotificationSerializer {
 
-    Map<String, Map<String, Map<String, String>>> serialize(Map<World, Map<EventType, Map<NotificationRule, Set<UUID>>>> data) {
-        final Map<String, Map<String, Map<String, String>>> serializedData = new HashMap<>();
-        for (final Map.Entry<World, Map<EventType, Map<NotificationRule, Set<UUID>>>> entry : data.entrySet()) {
-
-        }
+    Map<String, Map<String, Map<String, Set<String>>>> serialize(Map<World, Map<EventType, Map<String, Set<UUID>>>> data) {
+        return data.entrySet().stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey().getName(), // World name as key
+                        entry -> entry.getValue().entrySet().stream()
+                                .filter(Objects::nonNull)
+                                .collect(Collectors.toMap(
+                                        eventEntry -> eventEntry.getKey().name(), // EventType name
+                                        eventEntry -> eventEntry.getValue().entrySet().stream()
+                                                .filter(Objects::nonNull)
+                                                .collect(Collectors.toMap(
+                                                        ruleEntry -> ruleEntry.getKey().getClass().getSimpleName(), // Rule name
+                                                        ruleEntry -> ruleEntry.getValue().stream()
+                                                                .filter(Objects::nonNull)
+                                                                .map(UUID::toString) // Convert UUIDs to strings
+                                                                .collect(Collectors.toSet())
+                                                ))
+                                ))
+                ));
     }
 
-    Map<World, Map<EventType, Map<NotificationRule, Set<UUID>>>> deserialize() {
-
+    Map<World, Map<EventType, Map<String, Set<UUID>>>> deserialize(Map<String, Map<String, Map<String, Set<String>>>> data) {
+        return data.entrySet().stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.toMap(
+                        entry -> Bukkit.getWorld(entry.getKey()), // World name as key
+                        entry -> entry.getValue().entrySet().stream()
+                                .filter(Objects::nonNull)
+                                .collect(Collectors.toMap(
+                                        eventEntry -> EventType.valueOf(eventEntry.getKey()), // EventType name
+                                        eventEntry -> eventEntry.getValue().entrySet().stream()
+                                                .filter(Objects::nonNull)
+                                                .collect(Collectors.toMap(
+                                                        Map.Entry::getKey, // Rule name
+                                                        ruleEntry -> ruleEntry.getValue().stream()
+                                                                .filter(Objects::nonNull)
+                                                                .map(UUID::fromString) // Convert strings to UUIDs
+                                                                .collect(Collectors.toSet())
+                                                ))
+                                ))
+                ));
     }
 }
--- src/main/java/ru/vladimir/votvproduction/event/modules/notification/storage/PlayerNotificationService.java
+++ src/main/java/ru/vladimir/votvproduction/event/modules/notification/storage/PlayerNotificationService.java
@@ -1,4 +1,51 @@
 package ru.vladimir.votvproduction.event.modules.notification.storage;
 
+import lombok.RequiredArgsConstructor;
+import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
+import ru.vladimir.votvproduction.event.EventType;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+@RequiredArgsConstructor
 public class PlayerNotificationService {
+    private final PlayerNotificationStorage storage;
+    private final PlayerNotificationSerializer serializer;
+    private Map<World, Map<EventType, Map<String, Set<UUID>>>> data = new HashMap<>();
+
+    public void init() {
+        storage.save();
+        updateData();
+    }
+
+    public void updateStorage() {
+        storage.store(serializer.serialize(data));
+    }
+
+    public void updateData() {
+        data = serializer.deserialize(storage.retrieve());
+    }
+
+    @NotNull
+    public Map<EventType, Map<String, Set<UUID>>> getDataOfWorld(World world) {
+        if (!data.containsKey(world)) return Map.of();
+        else return Map.copyOf(data.get(world));
+    }
+
+    @NotNull
+    public Map<String, Set<UUID>> getDataOfWorldEvent(World world, EventType eventType) {
+        final Map<EventType, Map<String, Set<UUID>>> eventMap = getDataOfWorld(world);
+        if (!eventMap.containsKey(eventType)) return Map.of();
+        return eventMap.get(eventType);
+    }
+
+    @NotNull
+    public Set<UUID> getDataOfWorldEventRule(World world, EventType eventType, String rule) {
+        final Map<String, Set<UUID>> rulePlayerIds = getDataOfWorldEvent(world, eventType);
+        if (!rulePlayerIds.containsKey(rule)) return Set.of();
+        return rulePlayerIds.get(rule);
+    }
 }
