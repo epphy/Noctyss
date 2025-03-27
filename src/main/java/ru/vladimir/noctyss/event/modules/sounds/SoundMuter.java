bda2be94b83d4869dbf7a8a7916fc77a2d0677bd
Vladimir <gamedimaspro@gmail.com>
2025-03-27 16:13:15 +0400

    Update

--- src/main/java/ru/vladimir/noctyss/event/modules/sounds/SoundMuter.java
+++ src/main/java/ru/vladimir/noctyss/event/modules/sounds/SoundMuter.java
@@ -1,33 +1,14 @@
 package ru.vladimir.noctyss.event.modules.sounds;
 
 import lombok.RequiredArgsConstructor;
-import org.bukkit.GameMode;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
-import ru.vladimir.noctyss.event.Controllable;
+import org.bukkit.event.Listener;
 
 @RequiredArgsConstructor
-public class SoundMuter implements SoundManager, Controllable {
-    private static final long DELAY = 0L;
-    private static final long FREQUENCY = 20L;
+public class SoundMuter implements SoundManager, Listener {
     private final World world;
 
-    @Override
-    public void start() {
-
-    }
-
-    private void processPlayers() {
-        for (final Player player : world.getPlayers()) {
-            if (player.isDead()) continue;
-            if (player.getGameMode() == GameMode.SPECTATOR) continue;
-
-            player.stopAllSounds();
-        }
-    }
-
-    @Override
-    public void stop() {
+    private void on() {
 
     }
 }
