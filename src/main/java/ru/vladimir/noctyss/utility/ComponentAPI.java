935b8703475b5017ed969d6b1481f5011483a423
Vladimir <gamedimaspro@gmail.com>
2025-04-02 23:27:22 +0400

    Another attempt to fix colour processing

---
+++ src/main/java/ru/vladimir/noctyss/utility/ComponentAPI.java
@@ -0,0 +1,20 @@
+package ru.vladimir.noctyss.utility;
+
+import lombok.experimental.UtilityClass;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+@UtilityClass
+public class ComponentAPI {
+    private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+
+    public static Component parseColor(String message) {
+        // Convert legacy codes (&) to MiniMessage format
+        message = message.replaceAll("&([0-9a-fA-Fk-oK-OrR])", "<$1>");
+
+        // Convert hex codes {#xxxxxx} to MiniMessage format
+        message = message.replaceAll("\\{#([0-9A-Fa-f]{6})}", "<color:#$1>");
+
+        return MINI_MESSAGE.deserialize(message);
+    }
+}
